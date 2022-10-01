51700010622ba5344415f1698db4edd35d4898cb
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2022-10-01 16:52:05 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -17,7 +17,13 @@ public class Main {
 
     /* ****** Studerande kod till Create operation #1 ****** */
 
-
+      List<Integer> numbers = new ArrayList<>();
+      
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    
+    
 
 
 
